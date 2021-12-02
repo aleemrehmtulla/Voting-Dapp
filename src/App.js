583858e2b2db3033de7b7d10eef2067cdc7aa655
Aleem Rehmtulla <aleem@rehmtulla.com>
2021-12-02 11:49:52 -0500

    Connect Wallet custom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,75 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import President from "./components/President";
 import Trump from './assets/pictures/trump.jpg';
 import Biden from './assets/pictures/biden.jpg';
+import { ethers } from "ethers";
+import Button from 'react-bootstrap/Button';
+import abi from "./utils/Voting.json";
 
-function App() {
-  return (
+
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0xB78F65F89cf219d4CEa869f82B00CAaF56b45810";
+  const contractABI = abi.abi;
+  const network = "rinkeby";
   
 
-        <div className="rowC">
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        return account;
+      } else {
+        console.log("No authorized account found");
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      return accounts[0];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="mainContainer">
+      <div className="rowC">
           <div className="padding: 10rem">
             <President
             name="Joe Biden"
@@ -25,8 +86,29 @@ function App() {
           </div>
         </div>
    
-
+        
+        {/* Connect Wallet */}
+        <div className="topcorner">
+           <Button  onClick={connectWallet} variant="primary">
+             {!currentAccount && (
+                <span>Connect Wallet to {network}</span>
+              )}  
+              {currentAccount && (
+                
+                <span>Connected to {currentAccount}</span>
+              )}
+            </Button>
+          </div>
+       
+      </div>
+    
   );
-}
+};
 
 export default App;
+
+
+
+
+
+

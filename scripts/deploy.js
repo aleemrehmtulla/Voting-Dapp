6566f7cd78d6dd52994eb6ad50adaca8087a12a8
Aleem Rehmtulla <aleem@rehmtulla.com>
2021-12-03 11:48:07 -0500

    update how we deploy to new contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,12 @@
 const main = async () => {
   require('dotenv').config()
   const voteContractFactory = await hre.ethers.getContractFactory('VoteDapp');
-  const voteContract = await voteContractFactory.deploy({
-    value: hre.ethers.utils.parseEther('0.001'),
-  });
+  const voteContract = await voteContractFactory.deploy({});
 
   await voteContract.deployed();
 
   console.log('Contract address: ', voteContract.address);
+  console.log("https://etherscan.io/address/" + voteContract.address);
 };
 
 const runMain = async () => {

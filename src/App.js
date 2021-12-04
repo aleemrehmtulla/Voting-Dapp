ea0a5fed3df38a90a1ebcb67ce5cbd3bea0d237c
Aleem Rehmtulla <aleem@rehmtulla.com>
2021-12-03 20:43:18 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Trump from "./assets/pictures/trump.jpg";
 import Biden from "./assets/pictures/biden.jpg";
 import { ethers } from "ethers";
 import Button from "react-bootstrap/Button";
-import ProgressBar from "react-bootstrap/ProgressBar";
 import abi from "./utils/Voting.json";
 import Card from "react-bootstrap/Card";
-import styled from "styled-components";
+import Swal from "sweetalert2";
 import { PieChart } from 'react-minimal-pie-chart';
 
 const App = () => {
@@ -16,18 +13,14 @@ const App = () => {
   const contractAddress = "0xDf455f74042d20DF916054b55e851346Eca10839";
   const contractABI = abi.abi;
   const network = "rinkeby";
-  const [voteChoice, setVoteChoice] = useState([]);
-  const [allVotes, setAllVotes] = useState([]);
   const [totalTrump, setTotalTrump] = useState(0);
   const [totalBiden, setTotalBiden] = useState(0);
   const [totalVotes, setTotalVotes] = useState(0);
 
 
-
   const setVotes = async () => {
     try {
       const { ethereum } = window;
-
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
@@ -42,25 +35,19 @@ const App = () => {
         setTotalBiden(totalBiden.toNumber());
         setTotalTrump(totalTrump.toNumber());
         setTotalVotes(totalVotes.toNumber());
-        console.log("Retrieving vote counts...");
-        console.log("Total vote count:", totalVotes.toNumber());
-        console.log("Total trump count:", totalTrump.toNumber());
-        console.log("Total biden count:", totalBiden.toNumber());
-
-
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
       console.log(error);
+      Swal.fire('You must be connected to Rinkeby ETH testnet for this dapp to function.').then((result) => {
+        if (result.isConfirmed) {
+          connectWallet();
+        }
+      })
     }
   };
   setVotes();
-
-  function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-  }
-
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -71,7 +58,6 @@ const App = () => {
       } else {
         console.log("We have the ethereum object", ethereum);
       }
-
       const accounts = await ethereum.request({ method: "eth_accounts" });
 
       if (accounts.length !== 0) {
@@ -99,24 +85,17 @@ const App = () => {
           contractABI,
           signer
         );
-        let totalVotes = await voteDappContract.getTotalVotes();
-        let totalTrump = await voteDappContract.getTotalTrump();
-        let totalBiden = await voteDappContract.getTotalBiden();
-        console.log("Retrieving vote counts...");
-        console.log("Total vote count:", totalVotes.toNumber());
-        console.log("Total trump count:", totalTrump.toNumber());
-        console.log("Total biden count:", totalBiden.toNumber());
-
 
         const voteTxn = await voteDappContract.votetrump(
           "This is vote for trump by " + currentAccount,
           { gasLimit: 420690 }
         );
 
-
         console.log("Mining...", voteTxn.hash);
         await voteTxn.wait();
         console.log("Done! Check it here:", "https://rinkeby.etherscan.io/tx/" + voteTxn.hash);
+        setVotes();
+
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -138,27 +117,16 @@ const App = () => {
           contractABI,
           signer
         );
-        let totalVotes = await voteDappContract.getTotalVotes();
-        let totalTrump = await voteDappContract.getTotalTrump();
-        let totalBiden = await voteDappContract.getTotalBiden();
-        console.log("Retrieving vote counts...");
-        console.log("Total vote count:", totalVotes.toNumber());
-        console.log("Total trump count:", totalTrump.toNumber());
-        console.log("Total biden count:", totalBiden.toNumber());
-
-        
-
 
         const voteTxn = await voteDappContract.votebiden(
           "This is vote for biden by " + currentAccount,
           { gasLimit: 420690 }
         );
 
-
         console.log("Mining...", voteTxn.hash);
         await voteTxn.wait();
         console.log("Done! Check it here:", "https://rinkeby.etherscan.io/tx/" + voteTxn.hash);
-
+        setVotes();
 
       } else {
         console.log("Ethereum object doesn't exist!");
@@ -169,7 +137,6 @@ const App = () => {
   };
 
   
-
   // Connect Wallet function
   const connectWallet = async () => {
     try {
@@ -188,9 +155,6 @@ const App = () => {
       console.log(error);
     }
   };
-
-
-
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -223,13 +187,12 @@ const App = () => {
           <p><b>Total: {totalVotes} | Trump: {totalTrump} | Biden: {totalBiden}</b></p>
         <PieChart
             data={[
-            { title: 'Trump', value: totalTrump, color: '#e31c3d' },
-             { title: 'Biden', value: totalBiden, color: '#3174ce' },
+            { title: 'Trump', value: totalTrump, color: '#DC3444' },
+             { title: 'Biden', value: totalBiden, color: '#0C6DFD' },
 
-            ]}/>;
+            ]}/>
         </div>
         <div className="space" />
-        
         <div className="padding: 10rem">
           <Card style={{ width: "18rem" }}>
             <Card.Img variant="top" src={Trump} />
@@ -241,20 +204,15 @@ const App = () => {
                 reelected, trump will make america great again. He aims to
                 create a non-rigged election with blockchain"
               </Card.Text>
-              <Button variant="primary" onClick={voteTrump}>
+              <Button variant="danger" onClick={voteTrump}>
                 Vote For Donald Trump
               </Button>
             </Card.Body>
           </Card>
-          
-
-         
-
         </div>
         <div className="space" />
         <div>
-
-</div>
+      </div>
       </div>
 
       {/* Connect Wallet */}
